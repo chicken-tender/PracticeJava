536a4a428214f26608bbfa4224fd7f7418cba18f
ygm815 <ygm815@naver.com>
2023-01-14 19:42:41 +0900

    BufferedReader로 변경

--- src/백준15552번문제/BaekJoon15552.java
+++ src/백준15552번문제/BaekJoon15552.java
@@ -1,15 +1,18 @@
 package 백준15552번문제;
-import java.util.Scanner;
+
+import java.io.*;
 
 public class BaekJoon15552 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        for(int i = 0; i < num; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            sc.nextLine();
-            System.out.println(x + y);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++) {
+            String s = br.readLine();
+            int a = Integer.parseInt(s.split(" ")[0]);
+            int b = Integer.parseInt(s.split(" ")[1]);
+            bw.write(a+b+"\n");
         }
+        bw.flush();
     }
 }
