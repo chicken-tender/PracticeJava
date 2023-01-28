bfc68c1eef0b2819840748f0ffdb4740bc1f3eab
ygm815 <ygm815@naver.com>
2023-01-28 11:23:46 +0900

    .

--- src/백준1157번문제/Main.java
+++ src/백준1157번문제/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int[] alphabet = new int[26];
-        char result = 0;
+        char rst = 0;
         Scanner sc = new Scanner(System.in);
         String word = sc.next();
         for(int i = 0; i < word.length(); i++) {
@@ -14,12 +14,12 @@ public class Main {
         }
         int max = 0;
         for(int i = 0; i < alphabet.length; i++) {
-            if(alphabet[i] == max) result = '?';
+            if(alphabet[i] == max) rst = '?';
             else if (alphabet[i] > max) {
                 max = alphabet[i];
-                result = (char)('A' + i);
+                rst = (char)('A' + i);
             }
         }
-        System.out.println(result);
+        System.out.println(rst);
     }
 }
