a0b90ba5e326ec78ce2642c8b52e180542eae121
양경미 <ygm815@naver.com>
2023-03-09 18:57:23 +0900

    230309

---
+++ src/백준11718번문제/Main.java
@@ -0,0 +1,13 @@
+package 백준11718번문제;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String n = sc.nextLine();
+            System.out.println(n);
+        }
+    }
+}
